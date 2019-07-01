b27213c8cd1e207ba46c5799a05ec1af4c6e2b83
david collett <davida.collett95@gmail.com>
2019-07-01 17:31:58 -0600

    super done

--- Part2/index.js
+++ Part2/index.js
@@ -6,5 +6,12 @@ function setCard() {
     card.style.color = colorInput.value
 }
 function reset() {
-    
+    let card = document.getElementById('diamonds')
+    let card2 = document.getElementById('clubs')
+    let card3 = document.getElementById('hearts')
+    let card4 = document.getElementById('spades')
+    card.style.color = null
+    card2.style.color = null
+    card3.style.color = null
+    card4.style.color = null
 }
