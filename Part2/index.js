be08d2afa3e55e63300bc47accabaa8df6138741
david collett <davida.collett95@gmail.com>
2019-07-01 17:16:43 -0600

    super done

---
+++ Part2/index.js
@@ -0,0 +1,10 @@
+const idInput = document.getElementById('idInput')
+const colorInput = document.getElementById('colorInput')
+
+function setCard() {
+    let card = document.getElementById(idInput.value)
+    card.style.color = colorInput.value
+}
+function reset() {
+    
+}
